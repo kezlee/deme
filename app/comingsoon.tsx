e2e252a70149c4f43da7c6dcc753cb41f47e2954
Kez Lee <kez.lee@outlook.com>
2026-06-23 20:19:20 +0800

    added coming soon

---
+++ app/comingsoon.tsx
@@ -0,0 +1,11 @@
+import "./globals.css";
+
+export default function ComingSoon() {
+  return (
+    <main className="coming-soon">
+      <header className="nav">
+        <div className="logo">DEMË</div>
+      </header>
+    </main>
+  );
+}
