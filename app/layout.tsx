a199a6b467306a0d5545c2bab18302d765b30e32
Kez Lee <kez.lee@outlook.com>
2026-09-23 16:05:45 +0800

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Bebas_Neue, Roboto, Cutive_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const bebasNeue = Bebas_Neue({
   weight: "400",
@@ -32,6 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${bebasNeue.variable} ${roboto.variable} ${cutiveMono.variable}`}>
+      <Analytics/> 
       <body>{children}</body>
     </html>
   );
